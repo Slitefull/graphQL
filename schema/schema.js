bb786200bfe0ee923e46f6c3f657b67beead8da8
Maksym Tytechko <viptitechko1@gmail.com>
2022-04-06 15:43:47 +0300

    feat: added new collection

--- schema/schema.js
+++ schema/schema.js
@@ -9,7 +9,7 @@ const {
 
 const movies = [
   {
-    id: 1,
+    id: '1',
     name: 'Pulp fiction',
     genre: 'Criminal',
   },
@@ -22,23 +22,44 @@ const movies = [
     id: 3,
     name: 'Interstellar',
     genre: 'Future',
-  },
-  {
+  }, {
     id: '4',
     name: 'Batman',
     genre: 'Super Heroes',
   },
 ];
 
+const directors = [
+  {
+    id: '1',
+    name: 'Quentin Tarantino',
+    age: 55,
+  },
+  {
+    id: '1',
+    name: 'Michael Redford',
+    age: 72,
+  },
+  {
+    id: '1',
+    name: 'James Mc',
+    age: 51,
+  },
+  {
+    id: '1',
+    name: 'Gui Ritchie',
+    age: 50,
+  },
+];
+
 const MovieType = new GraphQLObjectType({
-    name: 'Movie',
-    fields: () => ({
-      id: { type: GraphQLID },
-      name: { type: GraphQLString },
-      genre: { type: GraphQLString },
-    })
-  }
-);
+  name: 'Movie',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  })
+});
 
 const Query = new GraphQLObjectType({
   name: 'Query',
--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schema/schema');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = express();
 const PORT = 3005;
